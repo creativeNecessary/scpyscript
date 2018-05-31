21da44f4d1e8a8bad02f15a3d526fd028cf2fbe9
eric <eirc@favalue.cn>
2018-05-31 18:23:39 +0800

    update Galaxy guide

--- CitizenWikiRobot/update_ships.py
+++ CitizenWikiRobot/update_ships.py
@@ -4,6 +4,8 @@ from CitizenWikiRobot.MysqlHelper import MysqlHelper
 from CitizenWikiRobot.comm_link_spectrum_dispatch import get_galactic_guide
 
 if __name__ == '__main__':
+    mysql_helper = MysqlHelper()
+    mysql_helper.clear_img_ship_equipment()
     get_ships()
     # init_road_map()
     # get_galactic_guide()
