4eb786d471fa787af0bfa755fea58f948454e62d
eric <eirc@favalue.cn>
2018-06-06 12:08:43 +0800

    update Galaxy guide

--- CitizenWikiRobot/update_apk_version.py
+++ CitizenWikiRobot/update_apk_version.py
@@ -3,6 +3,10 @@ from CitizenWikiRobot.roadmap import init_road_map
 from CitizenWikiRobot.MysqlHelper import MysqlHelper
 from CitizenWikiRobot.comm_link_spectrum_dispatch import get_galactic_guide
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 if __name__ == '__main__':
     my = MysqlHelper()
     my.update_apk(2, '1.0.1', "新增功能: 飞船浏览筛选,银河指南系列文章", 'sc_date_view_1.0.1-release.apk')
