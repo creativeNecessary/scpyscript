3aa93a56efbb3362e08494b889943444e56758e2
eric <eirc@favalue.cn>
2018-10-11 16:37:15 +0800

    update app version

--- CitizenWikiRobot/update_apk_version.py
+++ CitizenWikiRobot/update_apk_version.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     my = MysqlHelper()
     # my.update_apk(2, '1.0.1', "新增功能: 飞船浏览筛选,银河指南系列文章", 'sc_date_view_1.0.1-release.apk')
     # my.update_apk(3, '1.0.2', "1.飞船搜索 \n2.飞船列表优化 \n3.修复bug  \n下载地址 : https://pan.baidu.com/s/18W7x95GsZ1ME8nyi2AlmiA", 'sc_data_view_1.0.2-release.apk',"test","https://pan.baidu.com/s/18W7x95GsZ1ME8nyi2AlmiA")
-    my.update_apk(4, '1.0.3', "1.修复bug  \n下载地址 : https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA", 'sc_data_view_1.0.2-release.apk',"test","https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA")
+    my.update_apk(4, '1.0.3', "1.修复bug  \n下载地址 : https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA", 'sc_data_view_1.0.3-release.apk',"test","https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA")
