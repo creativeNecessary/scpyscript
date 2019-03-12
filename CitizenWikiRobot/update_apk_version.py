2c724e11f900a50d5b4f5a7be03919fa68338c22
eric <eirc@favalue.cn>
2019-03-12 17:07:49 +0800

    update ship price

--- CitizenWikiRobot/update_apk_version.py
+++ CitizenWikiRobot/update_apk_version.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     # my.update_apk(2, '1.0.1', "新增功能: 飞船浏览筛选,银河指南系列文章", 'sc_date_view_1.0.1-release.apk')
     # my.update_apk(3, '1.0.2', "1.飞船搜索 \n2.飞船列表优化 \n3.修复bug  \n下载地址 : https://pan.baidu.com/s/18W7x95GsZ1ME8nyi2AlmiA", 'sc_data_view_1.0.2-release.apk',"test","https://pan.baidu.com/s/18W7x95GsZ1ME8nyi2AlmiA")
     # my.update_apk(4, '1.0.3', "1.修复bug  \n下载地址 : https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA", 'sc_data_view_1.0.3-release.apk',"test","https://pan.baidu.com/s/1jve5pn1sup-q6GiautlagA")
-    my.update_apk(5, '1.0.4', "1.添加飞船价格 \n 2.由于签名由jar签名升级为全包签名,所以此次更新不支持直接安装,需要将app卸载重装! \n点击下载会将新版apk下载到 路径\n /storage/emulated/0/Android/data/sc.com.scdataview/cache/NetCache/UpdateApk文件夹中\n网盘下载地址 : https://pan.baidu.com/s/1-KLcdAxmZNOqhlEri6rZiA", 'sc_data_view_1.0.4-release.apk',"test","https://pan.baidu.com/s/1-KLcdAxmZNOqhlEri6rZiA")
+    my.update_apk(5, '1.0.4', "1.添加飞船价格 \n 2.由于签名由jar签名升级为全Apk签名,所以此次更新不支持直接安装,需要将app卸载重装! 建议使用网盘下载安装\n网盘下载地址 : https://pan.baidu.com/s/1-KLcdAxmZNOqhlEri6rZiA", 'sc_data_view_1.0.4-release.apk',"test","https://pan.baidu.com/s/1-KLcdAxmZNOqhlEri6rZiA")
